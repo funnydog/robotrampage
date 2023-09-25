37b7d97effd17cabfd41baa857756be48fba5858
funnydog <funnydog@users.noreply.github.com>
2023-09-25 06:39:06 +0200

    Initial commit.

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode . ((flycheck-clang-language-standard . "c++17")
+	      (flycheck-clang-include-path . ("." "/usr/include/SDL2/" "/usr/include/freetype2" "/usr/include/AL")))))
---
+++ src/robotrampage.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define PROJECT_NAME "robotrampage"
+
+int main(int argc, char **argv)
+{
+	if(argc != 1)
+	{
+		std::cout << argv[0] << "takes no arguments.\n";
+		return 1;
+	}
+	std::cout << "This is project " << PROJECT_NAME << ".\n";
+	return 0;
+}
