2b804dd9de53fa1ca2cec86a67254463fa384702
funnydog <funnydog@users.noreply.github.com>
2023-09-26 20:07:37 +0200

    Add the Sprite class.

---
+++ src/sprite.cpp
@@ -0,0 +1,194 @@
+#include "sprite.hpp"
+
+#include "rendertarget.hpp"
+
+Sprite::Sprite(
+	const Texture &texture,
+	FloatRect initialFrame,
+	glm::vec2 location,
+	glm::vec2 velocity)
+	: mTexture(texture)
+	, mTextureSize(texture.getSize())
+	, mFrameSize(initialFrame.size)
+	, mTintColor(Color::White)
+	, mFrameIndex(0)
+	, mFrameElapsed(0.f)
+	, mFrameDelay(0.1f)
+	, mLocation(location)
+	, mVelocity(velocity)
+	, mRotation(0.f)
+	, mCollisionRadius(0.f)
+	, mBoundingPadding(0.f)
+{
+	addFrame(initialFrame);
+}
+
+glm::vec2
+Sprite::getLocation() const
+{
+	return mLocation;
+}
+
+void
+Sprite::setLocation(glm::vec2 location)
+{
+	mLocation = location;
+}
+
+glm::vec2
+Sprite::getVelocity() const
+{
+	return mVelocity;
+}
+
+void
+Sprite::setVelocity(glm::vec2 velocity)
+{
+	mVelocity = velocity;
+}
+
+Color
+Sprite::getTintColor() const
+{
+	return mTintColor;
+}
+
+void
+Sprite::setTintColor(Color color)
+{
+	mTintColor = color;
+}
+
+float
+Sprite::getRotation() const
+{
+	return mRotation;
+}
+
+void
+Sprite::setRotation(float rotation)
+{
+	mRotation = rotation;
+}
+
+unsigned
+Sprite::getFrameIndex() const
+{
+	return mFrameIndex;
+}
+
+void
+Sprite::setFrameIndex(unsigned index)
+{
+	mFrameIndex = index % mFrames.size();
+}
+
+float
+Sprite::getFrameDelay() const
+{
+	return mFrameDelay;
+}
+
+void
+Sprite::setFrameDelay(float delay)
+{
+	mFrameDelay = delay;
+}
+
+glm::vec2
+Sprite::getCenter() const
+{
+	return mFrameSize * 0.5f + mLocation;
+}
+
+const FloatRect&
+Sprite::getSource() const
+{
+	return mFrames[mFrameIndex];
+}
+
+FloatRect
+Sprite::getDestination() const
+{
+	return { mLocation, mFrameSize };
+}
+
+FloatRect
+Sprite::getBoundingBox() const
+{
+	return {
+		mLocation + mBoundingPadding,
+		mFrameSize - mBoundingPadding * 2.f
+	};
+}
+
+bool
+Sprite::isBoxColliding(const FloatRect &other) const
+{
+	return getBoundingBox().intersect(other);
+}
+
+bool
+Sprite::isCircleColliding(glm::vec2 otherCenter, float otherRadius)
+{
+	auto radiusSum = mCollisionRadius+otherRadius;
+	otherCenter -= getCenter();
+	return otherCenter.x * otherCenter.x + otherCenter.y * otherCenter.y <
+		radiusSum * radiusSum;
+}
+
+float
+Sprite::getCollisionRadius() const
+{
+	return mCollisionRadius;
+}
+
+void
+Sprite::setCollisionRadius(float radius)
+{
+	mCollisionRadius = radius;
+}
+
+void
+Sprite::addFrame(const FloatRect &rect)
+{
+	mFrames.emplace_back(rect.pos / mTextureSize, rect.size / mTextureSize);
+}
+
+void
+Sprite::update(float dt)
+{
+	mFrameElapsed += dt;
+	if (mFrameElapsed >= mFrameDelay)
+	{
+		mFrameElapsed -= mFrameDelay;
+		mFrameIndex++;
+		if (mFrameIndex >= mFrames.size())
+		{
+			mFrameIndex = 0;
+		}
+	}
+	mLocation += mVelocity * dt;
+}
+
+void
+Sprite::draw(RenderTarget &target)
+{
+	auto srcRect = getSource();
+	auto dstRect = getDestination();
+	if (mRotation != 0.f)
+	{
+		target.blitQuad(
+			mTexture,
+			srcRect,
+			dstRect,
+			mTintColor,
+			mRotation,
+			mFrameSize * 0.5f,
+			1.f);
+	}
+	else
+	{
+		target.blitQuad(mTexture, srcRect, dstRect, mTintColor);
+	}
+}
---
+++ src/sprite.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <vector>
+
+#include <glm/glm.hpp>
+
+#include "color.hpp"
+#include "rect.hpp"
+
+class RenderTarget;
+class Texture;
+
+class Sprite
+{
+public:
+	Sprite(const Texture &texture, FloatRect initialFrame, glm::vec2 location, glm::vec2 velocity);
+
+	glm::vec2 getLocation() const;
+	void setLocation(glm::vec2 location);
+
+	glm::vec2 getVelocity() const;
+	void setVelocity(glm::vec2 velocity);
+
+	Color getTintColor() const;
+	void setTintColor(Color color);
+
+	float getRotation() const;
+	void setRotation(float rotation);
+
+	unsigned getFrameIndex() const;
+	void setFrameIndex(unsigned frame);
+
+	float getFrameDelay() const;
+	void setFrameDelay(float delay);
+
+	glm::vec2 getCenter() const;
+	const FloatRect& getSource() const;
+	FloatRect getDestination() const;
+
+	FloatRect getBoundingBox() const;
+	bool isBoxColliding(const FloatRect &other) const;
+	bool isCircleColliding(glm::vec2 otherCenter, float otherRadius);
+
+	float getCollisionRadius() const;
+	void setCollisionRadius(float radius);
+
+	void addFrame(const FloatRect &rect);
+
+	virtual void update(float dt);
+	virtual void draw(RenderTarget &target);
+
+protected:
+	// drawable
+	const Texture &mTexture;
+	glm::vec2 mTextureSize;
+	std::vector<FloatRect> mFrames;
+	glm::vec2 mFrameSize;
+	Color mTintColor;
+
+	// animation
+	unsigned mFrameIndex;
+	float mFrameElapsed;
+	float mFrameDelay;
+
+	// position
+	glm::vec2 mLocation;
+	glm::vec2 mVelocity;
+	float mRotation;
+
+	// collision
+	float mCollisionRadius;
+	glm::vec2 mBoundingPadding;
+};
