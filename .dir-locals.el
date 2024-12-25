5681f87cf1c8bda84ea78fc110c82f4b167cfcda
funnydog <funnydog@users.noreply.github.com>
2024-12-25 07:21:40 +0100

    Raise the required C++ standard to C++ 20.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode . ((flycheck-clang-language-standard . "c++17")
+((c++-mode . ((flycheck-clang-language-standard . "c++20")
 	      (flycheck-clang-include-path . ("." "/usr/include/SDL2/" "/usr/include/freetype2" "/usr/include/AL")))))
