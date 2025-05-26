e93fd16086643a9e5d73e584bf7479e260e4fb36
funnydog <funnydog@users.noreply.github.com>
2025-05-26 09:30:44 +0200

    Add Utility::decodeUTF8() to decode a utf8 string to std::u32string.

--- src/utility.hpp
+++ src/utility.hpp
@@ -8,4 +8,5 @@ namespace Utility
 std::string loadFile(const std::filesystem::path &filename);
 int randomInt(int exclusiveMax);
 float randomFloat(float exclusiveMax);
+std::u32string decodeUTF8(std::string_view str);
 }
